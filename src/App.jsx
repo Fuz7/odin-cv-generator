94ae64525c6a6bc52109969b50896c9340ca2d1e
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-03-09 21:04:31 +0800

    add all necessary fonts & svgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './main.scss';
 
 function App() {
-  return <></>;
+  return (
+    <>
+
+    </>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-})
+   resolve: {
+    alias: {
+      '@fonts': '/src/assets/fonts',
+      '@svgs': '/src/assets/svgs'
+    },
+  },
+});
